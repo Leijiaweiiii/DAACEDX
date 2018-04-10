9a1eb31c851749d77fa7376fe455671d20b138b0
Boris Sorochkin <boris@nestlogic.com>
2018-04-10 22:17:53 +0300

    Store more fonts

---
+++ Anonymous_Pro_12pt.h
@@ -0,0 +1,5 @@
+/* Font data for Anonymous Pro 12pt */
+extern const uint_8 anonymousPro_12ptBitmaps[];
+extern const FONT_INFO anonymousPro_12ptFontInfo;
+extern const FONT_CHAR_INFO anonymousPro_12ptDescriptors[];
+
---
+++ robotoCondensed24pt.h
@@ -0,0 +1,5 @@
+/* Font data for Roboto Condensed 24pt */
+extern const uint_8 robotoCondensed_24ptBitmaps[];
+extern const FONT_INFO robotoCondensed_24ptFontInfo;
+extern const FONT_CHAR_INFO robotoCondensed_24ptDescriptors[];
+
