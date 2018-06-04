01b692d5d00c91c56183a3205aee4b2885ed858b
Boris Sorochkin <boris@nestlogic.com>
2018-06-04 21:11:33 +0300

    Increment version

--- DAACEDcommon.h
+++ DAACEDcommon.h
@@ -15,7 +15,7 @@
 #define STRMYCMP
 extern void strmycpy(char * to, const char * from);
 #endif
-#define FW_VERSION (0x02)
+#define FW_VERSION (0x03)
 // <editor-fold defaultstate="collapsed" desc="General">
 #ifndef _XTAL_FREQ
 #define _XTAL_FREQ            (64000000UL) //64MHz
