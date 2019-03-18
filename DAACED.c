926858b8b7872a944045408d2f8e66d66ebd28a4
Boris Sorochkin <boris@nestlogic.com>
2019-03-18 13:38:18 +0200

    Save sensitivity with correct field size

--- DAACED.c
+++ DAACED.c
@@ -774,7 +774,7 @@ void SetSens() {//Sensitivity
     if (s.selected) {
         Settings.Sensitivity = s.value;
         if (s.value != s.old_value) {
-            saveSettingsField(&Settings, &(Settings.Sensitivity), 1);
+            saveSettingsField(&Settings, &(Settings.Sensitivity), 2);
         }
     }
 }
