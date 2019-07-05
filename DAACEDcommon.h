fd28f3e0762ac6b3828316f8f997faf92fd69c69
Boris Sorochkin <boris@nestlogic.com>
2019-07-05 18:53:09 +0300

    Increment FW Version

--- DAACEDcommon.h
+++ DAACEDcommon.h
@@ -13,7 +13,7 @@
 #include "string.h"
 #include <time.h>
 
-#define FW_VERSION (0x29)
+#define FW_VERSION (42)
 // <editor-fold defaultstate="collapsed" desc="General">
 #ifndef _XTAL_FREQ
 #define _XTAL_FREQ            (64000000UL) //64MHz
