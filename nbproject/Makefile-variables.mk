a0ab9151bad6369340506f8457b671236eacf20d
Boris Sorochkin <boris@nestlogic.com>
2019-10-05 20:04:18 +0300

    Add project configuration for xc8 v2.10 compiler

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -11,6 +11,13 @@ CND_ARTIFACT_PATH_v2_05=dist/v2_05/production/DAACED.X.production.hex
 CND_PACKAGE_DIR_v2_05=${CND_DISTDIR}/v2_05/package
 CND_PACKAGE_NAME_v2_05=daaced.x.tar
 CND_PACKAGE_PATH_v2_05=${CND_DISTDIR}/v2_05/package/daaced.x.tar
+# v2_10 configuration
+CND_ARTIFACT_DIR_v2_10=dist/v2_10/production
+CND_ARTIFACT_NAME_v2_10=DAACED.X.production.hex
+CND_ARTIFACT_PATH_v2_10=dist/v2_10/production/DAACED.X.production.hex
+CND_PACKAGE_DIR_v2_10=${CND_DISTDIR}/v2_10/package
+CND_PACKAGE_NAME_v2_10=daaced.x.tar
+CND_PACKAGE_PATH_v2_10=${CND_DISTDIR}/v2_10/package/daaced.x.tar
 # v2_05_o configuration
 CND_ARTIFACT_DIR_v2_05_o=dist/v2_05_o/production
 CND_ARTIFACT_NAME_v2_05_o=DAACED.X.production.hex
