ccde6832aac25c855952324d0f220d537ceb81f3
Boris Sorochkin <boris@nestlogic.com>
2020-02-04 19:31:10 +0200

    FW_VERSION 71

--- DAACEDcommon.h
+++ DAACEDcommon.h
@@ -14,7 +14,7 @@
 #include <time.h>
 
 // Increment to reset settings after programming new version
-#define FW_VERSION (70)
+#define FW_VERSION (71)
 // <editor-fold defaultstate="collapsed" desc="General">
 #ifndef _XTAL_FREQ
 #define _XTAL_FREQ            (64000000UL) //64MHz
